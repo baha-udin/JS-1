a2792a61984734697232ce55f2157755a5964a8c
Muhamad Baha Udin <you@example.com>
2020-07-11 14:12:54 +0700

    update progress

---
+++ exercise12.js
@@ -0,0 +1,19 @@
+function balikString(str) {
+    var string = '';
+
+    for (var index = str.length - 1; index >= 0; index--) {
+        string += str[index];
+    }
+    return string;
+}
+
+console.log(balikString('hello world!'));function balikString(str) {
+    var string = '';
+
+    for (var index = str.length - 1; index >= 0; index--) {
+        string += str[index];
+    }
+    return string;
+}
+
+console.log(balikString('hello world!'));
