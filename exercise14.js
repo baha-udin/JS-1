9c069cfbd2a813dd121f3ecc797ca648b97e18c2
Muhamad Baha Udin <you@example.com>
2020-07-12 14:25:47 +0700

    update progress

---
+++ exercise14.js
@@ -0,0 +1,35 @@
+//function dengan parameter
+var printPrint = function() {
+    console.log("print")
+}
+printPrint()
+
+//function tanpa parameter
+var home = function(text) {
+    console.log("meja" + text + "kursi")
+}
+home("lemari")
+
+//function dengan parameter
+let restoran = function(text) {
+    console.log("koki" + text + "pengunjung")
+}
+restoran("pelayan")
+
+//cheatsheet buat function
+function kegiatan(text) {
+    console.log("makan" + text + "Tidur")
+}
+
+kegiatan("ngoding")
+
+//biasanya function mengembalikan nilai (return value) yg bisa
+//kita ambil berikutnya
+//jika kita ingin mendapatkan nilai yg dikembalikan, maka 
+//kita assign ke sebuah variabel DILUAR FUNCTION
+
+let timesTwo =  function(x) {
+    return x * 2;
+}; 
+var result = timesTwo(8)
+console.log(result)
